2db4b5038003466b83ff49c8147203db0e98557e
Anash Farhan <anashbusiness6@gmail.com>
2026-08-19 10:18:09 -0700

    updated javascript assignment

---
+++ section 1/Assignment 2/index.js
@@ -0,0 +1,17 @@
+//swapping numbers//
+
+let a = 100;
+let b = 9876;
+
+console.log("before swapping");
+console.log("a=", a);
+console.log("b=", b);
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("after swapping");
+console.log("a=", a);
+console.log("b=", b);
+
--- section 3/Assignment 15/index.js
+++ section 3/Assignment 15/index.js
@@ -1,2 +1,15 @@
 //electricity bill//
 
+let units = 250;
+let bill;
+
+if (units <= 100) {
+    bill = units * 5;
+} else if (units <= 200) {
+    bill = (100 * 5) + ((units - 100) * 7);
+} else {
+    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10);
+}
+
+console.log("Electricity Bill: ₹" + bill);
+
